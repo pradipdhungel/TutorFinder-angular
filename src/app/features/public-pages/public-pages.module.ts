1ac529d9b3afeb10be34035d1e8504383307f942
eme2119@gmail.com <eme2119@gmail.com>
2022-10-25 12:22:00 -0500

    tuto requirement form updated

--- src/app/features/public-pages/public-pages.module.ts
+++ src/app/features/public-pages/public-pages.module.ts
@@ -5,7 +5,7 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { HomeNavbarComponent } from './components/home-navbar/home-navbar.component';
 import { PublicPagesRoutingModule } from './public-pages-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { JobSearchComponent } from './components/job-search/job-search.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TutorRequirementsComponent } from './jobs/jobs.component';
@@ -23,11 +23,13 @@ import { TutorRequirementComponent } from './Tutor-Requirement-Page/tutor-requir
     FooterComponent,
     TutorRequirementsComponent,
     TutorRequirementComponent,
+    
   ],
   imports: [
     CommonModule,
     PublicPagesRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     BsDropdownModule.forRoot()
   ]
 })
