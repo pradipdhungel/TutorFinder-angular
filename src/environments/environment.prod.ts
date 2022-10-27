17e669b64a232348e5c783557e8ec7cc8d2fa8db
rajiv <gyawalirajiv@gmail.com>
2022-10-27 14:00:30 -0500

    Refactoring based on environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
   production: true,
-  appBrandName: 'My App',
-  devApiUrl: 'http://35.193.139.217'
+  appBrandName: 'Tutor Finder',
+  devApiUrl: 'http://localhost',
+  prodApiUrl: {
+    baseUrl: "http://34.173.24.247",
+    authentication: "/authentication",
+    tutorRequirement: "/tutorRequirement",
+    tutorRequirementSearch: "/tutorRequirementSearch",
+    reservation: "/reservation"
+  }
 };
